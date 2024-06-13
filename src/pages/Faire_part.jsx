124330274951e428bd25ece73b2bbec86c9b5cfc
= Yang Rosine <80988887+Myakii@users.noreply.github.com>
2024-06-13 14:46:48 +0200

    commit

--- src/pages/Faire_part.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Faire_part() {
-  return (
-    <div>Faire_part</div>
-  )
-}
--- src/pages/Livre_or.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Livre_or() {
-  return (
-    <div>Livre_or</div>
-  )
-}
